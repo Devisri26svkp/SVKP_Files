d077c6a0361ddc4f69582731bd2cc10c64b9de69
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 15:30:56 +0530

    Eighth commit

---
+++ svkp_college/src/basicsofjava/SimpleIfElseDemo.java
@@ -0,0 +1,17 @@
+package basicsofjava;
+
+public class SimpleIfElseDemo {
+
+	public static void main(String[] args) {
+	 int age=11;
+	 if(age>18)
+	 {
+		System.out.println("You are Eligible");
+	 }
+	 else
+	 {
+		 System.out.println("You are not Eligible ");
+	 }
+	}
+
+}
