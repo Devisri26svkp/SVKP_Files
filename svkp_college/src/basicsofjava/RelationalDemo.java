4697353ac5f75bcb2cfb2af677d12ce999e9fdca
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 16:40:43 +0530

    Nineteenth commit

---
+++ svkp_college/src/basicsofjava/RelationalDemo.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class RelationalDemo {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		System.out.println("a==b"+(a==b));//equal to//false
+		System.out.println("a!=b"+(a!=b));//not equal to//true
+		System.out.println("a>b"+(a>b));//greater than//true
+		System.out.println("a<b"+(a<b));//less than//false
+		System.out.println("a>=b"+(a>=b));//greater than or equal to//true
+		System.out.println("a<=b"+(a<=b));//less than orequal to//false
+	}
+
+}
