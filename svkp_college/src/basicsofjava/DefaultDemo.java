49a65769f66217a006df86d676caf2650bbb987a
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 15:08:19 +0530

    fifth commit

---
+++ svkp_college/src/basicsofjava/DefaultDemo.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+ class Demo{
+	 String strvalue;
+	 int a[];
+ }
+ public class DefaultDemo {
+
+	public static void main(String[] args) {
+		Demo d1=new Demo();
+		System.out.println(d1.strvalue);
+		System.out.println(d1.a);
+
+	}
+
+}
