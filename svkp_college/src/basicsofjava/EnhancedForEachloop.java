66ac17f6a35fdd0cf056c55b70b21822bf921dc8
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 16:13:00 +0530

    Fourteen commit

---
+++ svkp_college/src/basicsofjava/EnhancedForEachloop.java
@@ -0,0 +1,15 @@
+package basicsofjava;
+
+public class EnhancedForEachloop {
+
+	public static void main(String[] args) {
+		int num[]= {21,4,5,6,7,9,8};
+		for(int numbers:num)
+		{
+			System.out.println(numbers);
+			
+		}
+
+	}
+
+}
