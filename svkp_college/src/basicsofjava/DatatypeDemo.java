60911704e3f475a9076806070ac35e5fb09bb643
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 14:45:36 +0530

    Third commit

--- svkp_college/src/basicsofjava/DatatypeDemo.java
+++ svkp_college/src/basicsofjava/DatatypeDemo.java
@@ -6,7 +6,7 @@ public class DatatypeDemo {
 		//byte-1byte
 		byte byteMax=127;
 		byte byteMin=-128;
-		sysout
+		sysou  
         //short -2bytes
 		short shortMax=32767;
 		short shortMin=-32768;
@@ -14,7 +14,7 @@ public class DatatypeDemo {
 		//int-4bytes
 		int intMax=2147483647;
 		int intMin=-2147483648;
-		sysout
+		syso
 	}
 
 }
