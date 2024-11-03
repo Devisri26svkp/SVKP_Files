4004de1a255f03db62e12b023d42e1aa93d7675a
karum <karum@LAPTOP-8H83RG1O>
2024-11-03 15:59:40 +0530

    twelve commit

---
+++ svkp_college/src/basicsofjava/DoWhileDemo.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("Welcome to c2tc Session");
+			i++;
+		}while(i<=5);
+	}
+
+}
