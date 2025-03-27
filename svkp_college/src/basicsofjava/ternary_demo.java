7d877862955c658ea87802ec17ef6656041cc3b2
karum <karum@LAPTOP-8H83RG1O>
2025-03-27 16:24:02 +0530

    first commit

---
+++ svkp_college/src/basicsofjava/ternary_demo.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+
+public class ternary_demo {
+
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int max=(a>b)?a:b;
+		System.out.println("max value is : " + max);
+
+	}
+
+}
